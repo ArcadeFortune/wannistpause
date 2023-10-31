87a4f06f2d05a700538c3d279b82e8eaffca5ef3
ArcadeFortune <alessionights@gmail.com>
2023-10-31 14:52:40 +0100

    feat: can handle break times + better loading

--- src/Countdown.js
+++ src/Countdown.js
@@ -19,55 +19,92 @@ function renderTime ({ remainingTime }) {
 
 
 function getActiveInterval(currentTime, everyTimeStamp) {
+  let prevEnd = null;
+  let nextStart = null;
   let prevInterval = null;
   let found = false;
-  for (const interval of everyTimeStamp) {
+
+  for (let i = 0; i < everyTimeStamp.length; i++) {
+    const interval = everyTimeStamp[i];
     const [start, end] = interval.trim().split(' - ').map(t => moment(t, 'HH:mm'));
 
     if (found) {
-      return { current: prevInterval.current, next: { start, end } };
+      // console.log({ current: prevInterval, next: { start, end } })
+      return { current: prevInterval };
     }
 
+    // Check if currentTime is between the end of the previous interval and the start of the current.
+    if (prevEnd && currentTime.isBetween(prevEnd, start, null, '[)')) {
+      nextStart = start; // The start of the next interval after the gap.
+      break; // We found the gap, break the loop.
+    }
+
+    // Check if currentTime is within the current interval.
     if (currentTime.isBetween(start, end, null, '[)')) {
       found = true;
-      prevInterval = { current: { start, end }, next: null };
+      prevInterval = { start, end };
     }
+
+    // Update prevEnd for the next iteration to check gaps.
+    prevEnd = end;
   }
 
-  return null;
+  // check if the current time is after the last interval
+  if (found && !nextStart) {
+    console.log('is after the last interval')
+    return { current: prevInterval, next: null };
+  }
+
+  // does not work
+  // check if the current time is before the first interval
+  // if (!found && !prevEnd) {
+  //   console.log('is before the first interval')
+  //   return { current: null, next: null, gap: { after: nextStart } };
+  // }
+
+  
+  // If currentTime falls within a gap, return the gap information.
+  if (nextStart) {
+    // console.log({ current: { start: prevEnd, end: nextStart }, next: { start: nextStart }, gap: { before: prevEnd, after: nextStart } })
+    return { current: { start: prevEnd, end: nextStart } };
+  }
+
+  // If no conditions met, currentTime is not within any intervals or gaps.
+  return 0;
 }
 
 
 const CountdownComponent = ({everyTimeStamp}) => {
-  const [totalDuration, setTotalDuration] = useState(2700);  
-  const [timerFinished, setTimerFinished] = useState(false);
-  const [key, setKey] = useState(1);
+  const [totalDuration, setTotalDuration] = useState(null); // either 45 minutes or 10 minutes
+  const [activeInterval, setActiveInterval] = useState(null); // get the two timestamps of the current interval
+  const [remainingTime, setRemainingTime] = useState(null); // remaining time of the current interval [seconds]
+  const [timerFinished, setTimerFinished] = useState(false); // to procc the confetti
 
   useEffect(() => {
-    if (timerFinished) {
-      setTimeout(() => {
-        setTimerFinished(false);
-      }, 4000);
-    }
-  }, [totalDuration]);
+    if (everyTimeStamp.length === 0) return; // needs to load data
+    // const currentTime = moment('17:55', 'HH:mm') // for testing
+    const currentTime = moment(); // sets current time
+    setActiveInterval(getActiveInterval(currentTime, everyTimeStamp)); // finds current interval
+    if (!activeInterval) return; // needs to refresh setState
+    setTotalDuration(activeInterval.current.end.diff(activeInterval.current.start, 'seconds')) // sets the duration of the current interval either 45 minutes or 10 minutes
+    setRemainingTime(activeInterval.current.end.diff(currentTime, 'seconds')) // sets the remaining time of the current interval
+  }, [everyTimeStamp, timerFinished]);
+
+
+  if (everyTimeStamp.length === 0) return <div>Intranet laden...</div>;
+  if (activeInterval === 0) return <div>Kein Unterricht!</div>; // badly solved
+  if (!totalDuration) return <div>Zeit rechnen...</div>;
+  if (!activeInterval) return <div>Kein Unterricht!</div>;
 
   const handleComplete = () => {
     console.log('finished!')
-    setTimerFinished(true)
-    setKey((prevKey) => prevKey + 1)
-    setTotalDuration((maxDuration) => maxDuration + 1)
+    // setTotalDuration((maxDuration) => maxDuration + 1)
+
+    setTimerFinished(true)    
+    setTimeout(() => {
+      setTimerFinished(false);
+    }, 4000);
   };
-  
-  // const currentTime = moment('08:29', 'HH:mm')
-  const currentTime = moment()  
-  
-  var activeInterval = getActiveInterval(currentTime, everyTimeStamp);
-
-  if (everyTimeStamp.length === 0) return <div>Loading...</div>;
-  if (!activeInterval) return <div>Kein Unterricht!</div>;
-
-  const durationInSeconds = activeInterval.current.end.diff(currentTime, 'seconds');
-  
 
   return (
     <>
@@ -78,10 +115,9 @@ const CountdownComponent = ({everyTimeStamp}) => {
       colors={["#A30000", "#ff2626", "#F7B801", "#00d619", "#00d619"]}
       colorsTime={[2700*0.87, 2700*0.75, 2700*0.5, 2700*0.25, 0]}
       
-      key={key}
       duration={totalDuration}
-      initialRemainingTime={durationInSeconds}
-      // initialRemainingTime={5}
+      initialRemainingTime={remainingTime}
+      // initialRemainingTime={5} // for testing
       onComplete={handleComplete}
     >
       {renderTime}
