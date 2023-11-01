bd74a9bc950abda9899ca106a4a2be593b105f4a
ArcadeFortune <alessionights@gmail.com>
2023-11-01 10:31:30 +0100

    fix: npm run build tested

--- src/Countdown.js
+++ src/Countdown.js
@@ -3,75 +3,7 @@ import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import moment from 'moment';
 import './Countdown.css';
 import Confetti from 'react-confetti'
-
-
-function renderTime ({ remainingTime }) {
-  if (remainingTime === 0) return <div className="timer">Pause!</div>;
-
-  return (
-    <div className="timer">
-      <div className="text">Remaining</div>
-      <div className="value">{moment.utc(remainingTime*1000).format('mm:ss')}</div>
-      <div className="text">minutes</div>
-    </div>
-  );
-};
-
-
-function getActiveInterval(currentTime, everyTimeStamp) {
-  let prevEnd = null;
-  let nextStart = null;
-  let prevInterval = null;
-  let found = false;
-
-  for (let i = 0; i < everyTimeStamp.length; i++) {
-    const interval = everyTimeStamp[i];
-    const [start, end] = interval.trim().split(' - ').map(t => moment(t, 'HH:mm'));
-
-    if (found) {
-      // console.log({ current: prevInterval, next: { start, end } })
-      return { current: prevInterval };
-    }
-
-    // Check if currentTime is between the end of the previous interval and the start of the current.
-    if (prevEnd && currentTime.isBetween(prevEnd, start, null, '[)')) {
-      nextStart = start; // The start of the next interval after the gap.
-      break; // We found the gap, break the loop.
-    }
-
-    // Check if currentTime is within the current interval.
-    if (currentTime.isBetween(start, end, null, '[)')) {
-      found = true;
-      prevInterval = { start, end };
-    }
-
-    // Update prevEnd for the next iteration to check gaps.
-    prevEnd = end;
-  }
-
-  // check if the current time is after the last interval
-  if (found && !nextStart) {
-    console.log('is after the last interval')
-    return { current: prevInterval, next: null };
-  }
-
-  // does not work
-  // check if the current time is before the first interval
-  // if (!found && !prevEnd) {
-  //   console.log('is before the first interval')
-  //   return { current: null, next: null, gap: { after: nextStart } };
-  // }
-
-  
-  // If currentTime falls within a gap, return the gap information.
-  if (nextStart) {
-    // console.log({ current: { start: prevEnd, end: nextStart }, next: { start: nextStart }, gap: { before: prevEnd, after: nextStart } })
-    return { current: { start: prevEnd, end: nextStart } };
-  }
-
-  // If no conditions met, currentTime is not within any intervals or gaps.
-  return 0;
-}
+import { getActiveInterval, renderTime } from './importantFunctions';
 
 
 const CountdownComponent = ({everyTimeStamp}) => {
@@ -79,16 +11,17 @@ const CountdownComponent = ({everyTimeStamp}) => {
   const [activeInterval, setActiveInterval] = useState(null); // get the two timestamps of the current interval
   const [remainingTime, setRemainingTime] = useState(null); // remaining time of the current interval [seconds]
   const [timerFinished, setTimerFinished] = useState(false); // to procc the confetti
+  const [nextSubject, setNextSubject] = useState('dummy subject'); // to display the next subject
   const [a, setA] = useState(0); // to refresh the useState
 
   useEffect(() => {
     if (everyTimeStamp.length === 0) return; // needs to load data
-    // const currentTime = moment('08:39:55', 'HH:mm:ss') // for testing
+    // const currentTime = moment('08:35:55', 'HH:mm:ss') // for testing
     const currentTime = moment(); // sets current time
     setActiveInterval(getActiveInterval(currentTime, everyTimeStamp)); // finds current interval
 
-    if (!activeInterval)  { 
-      if (a > 5) setA((prevA) => prevA + 1);
+    if (!activeInterval) { 
+      if (a < 5) setA((prevA) => prevA + 1);
       return;
     } // needs to refresh setState
 
---
+++ src/importantFunctions.js
@@ -0,0 +1,67 @@
+import moment from "moment";
+import React from "react";
+
+
+export function renderTime({ remainingTime }) {
+  if (remainingTime === 0) return <div className="timer">Pause!</div>;
+
+  return (
+    <div className="timer">
+      <div className="text">Remaining</div>
+      <div className="value">{moment.utc(remainingTime * 1000).format('mm:ss')}</div>
+      <div className="text">minutes</div>
+    </div>
+  );
+}
+
+export function getActiveInterval(currentTime, everyTimeStamp) {
+  let prevEnd = null;
+  let nextStart = null;
+  let prevInterval = null;
+  let found = false;
+
+  for (let i = 0; i < everyTimeStamp.length; i++) {
+    const interval = everyTimeStamp[i];
+    const [start, end] = interval.trim().split(' - ').map(t => moment(t, 'HH:mm'));
+
+    if (found) {
+      // console.log({ current: prevInterval, next: { start, end } })
+      return { current: prevInterval };
+    }
+
+    // Check if currentTime is between the end of the previous interval and the start of the current.
+    if (prevEnd && currentTime.isBetween(prevEnd, start, null, '[)')) {
+      nextStart = start; // The start of the next interval after the gap.
+      break; // We found the gap, break the loop.
+    }
+
+    // Check if currentTime is within the current interval.
+    if (currentTime.isBetween(start, end, null, '[)')) {
+      found = true;
+      prevInterval = { start, end };
+    }
+
+    // Update prevEnd for the next iteration to check gaps.
+    prevEnd = end;
+  }
+
+  // check if the current time is after the last interval
+  if (found && !nextStart) {
+    return { current: prevInterval, next: null };
+  }
+
+  // does not work
+  // check if the current time is before the first interval
+  // if (!found && !prevEnd) {
+  //   console.log('is before the first interval')
+  //   return { current: null, next: null, gap: { after: nextStart } };
+  // }
+  // If currentTime falls within a gap, return the gap information.
+  if (nextStart) {
+    // console.log({ current: { start: prevEnd, end: nextStart }, next: { start: nextStart }, gap: { before: prevEnd, after: nextStart } })
+    return { current: { start: prevEnd, end: nextStart } };
+  }
+
+  // If no conditions met, currentTime is not within any intervals or gaps.
+  return 0;
+}
