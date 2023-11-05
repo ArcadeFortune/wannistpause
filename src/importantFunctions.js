66065789b44fefc47ea8ff74b82d0e5a17fa75db
ArcadeFortune <alessionights@gmail.com>
2023-11-05 17:53:36 +0100

    different text for free lessons

--- src/importantFunctions.js
+++ src/importantFunctions.js
@@ -68,7 +68,7 @@ export function getActiveInterval(currentTime, everyTimeStamp) {
 
 export function getNextSubject(timeIndex, timestamps, todaysSubjects, currentClass) {
   const currentClassList = (getCurrentClass(todaysSubjects, currentClass))
-  if (timeIndex > currentClassList.length - 1) return {subject: "Frei Stunde!"}; // if the current time is after the last lesson, return a free lesson
+  if (timeIndex > currentClassList.length - 1) return {subject: "FREI!!!"}; // if the current time is after the last lesson, return a free lesson
 
   // remove any deleted lessons from the returned object
   let deletedLessons = currentClassList[timeIndex].querySelectorAll('.ttp-mod-deleted')
@@ -78,7 +78,7 @@ export function getNextSubject(timeIndex, timestamps, todaysSubjects, currentCla
 
   console.log('Die nächste Lektion ist:', nextLessonArray)
   if (JSON.stringify(nextLessonArray) === '[""]') { // '[""]' this needs to be perfectly like this lol
-    return {subject: "Frei Stunde!"};
+    return {subject: "FREI!!!"};
   } else {
     return {subject: nextLessonArray[0], teacher: nextLessonArray[1], room: nextLessonArray[2]};
   }
