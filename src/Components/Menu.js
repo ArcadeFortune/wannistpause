ba6d451fef376e1822e51f628628a33ebd6252ae
ArcadeFortune <lamaa@bzz.ch>
2023-11-09 15:35:17 +0100

    feat: intranet button

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -7,6 +7,7 @@ export function Menu() {
   return (
     <>
       <div onClick={ksh.handleChangeClassClick}>Klasse Wechesln</div>
+      <div onClick={() => window.location.href = 'https://intranet.tam.ch/ksh/public/timetable/daily-class-schedule'}>Stundenplan</div> {/* Stundenplan */}
       <div onClick={() => alert('bro da staht coming soon')}>Coming Soon...</div> {/* Stundenplan */}
     </>
   )
