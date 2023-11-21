bb9c25be54ea01fe7b6e329e27e5452adf54e1d0
ArcadeFortune <alessionights@gmail.com>
2023-11-21 15:22:04 +0100

    finished about me page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,9 +5,12 @@ export default function AboutMe() {
         <br/>
         <span className="information">Über diese Website</span> <br/>
         <div className="text">
-          Ich bin ein {new Date().getFullYear() - 2005} jähriger der einst zu der KSH geht, in der IMS.<br/>
-          Wie du, habe ich immer Probleme gehabt, die Zeit zur nächsten Pause auszurechnen, sowie die nächste Stunde zu ermitteln. <br/>
-          Also habe ich diese Website erstellt, um mir und dir das Leben zu erleichtern.
+          Ich bin ein {new Date().getFullYear() - 2005}-jähriger der einst zu der KSH ging, in der IMS. (Jahrgang 21)<br/>
+          Wie du, hatte ich immer Probleme gehabt, die Zeit zur nächsten Pause auszurechnen, sowie die nächste Stunde zu ermitteln. <br/>
+          Also habe ich diese Website erstellt, um mir und dir das Leben zu erleichtern. <br/> <br/>
+          Die Stundenplan-daten werden vom <a href="https://intranet.tam.ch/ksh/public/timetable/daily-class-schedule" className="link">Intranet</a> geholt. <br/> <br/>
+          Wenn du ein Bug findest / mit programmieren willst, schau dir mein <a href="https://github.com/arcadefortune/wannistpause" className="link">GitHub</a> an<br/> <br/>
+          Ich habe auch ein wunderschönes Rechtsclick-Menu gebaut :)
         </div>
       </div>
     </div>
