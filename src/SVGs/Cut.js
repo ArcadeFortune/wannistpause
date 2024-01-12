4b7efc46a34f2b7e3fe943cdfd4ea9dea6c1f05b
ArcadeFortune <alessionights@gmail.com>
2024-01-12 21:29:31 +0100

    resolve #20: TeacherView implemented (not this commit)

--- src/SVGs/Cut.js
+++ src/SVGs/Cut.js
@@ -1,5 +1,4 @@
 import { IoIosCut } from "react-icons/io";
-import { copyText } from "../importantFunctions";
 
 export default function Cut({ text }) {
   return <IoIosCut onClick={async () => { await navigator.clipboard.writeText(text); console.log(`Schneide den Text '${text}'.`); }}/>
