6f95447530c1e17a67b3ca39f39a49872afa99e8
ArcadeFortune <alessionights@gmail.com>
2024-01-15 19:04:06 +0100

    testing with vercel's serverless functions

--- src/SVGs/CopyLink.js
+++ src/SVGs/CopyLink.js
@@ -1,6 +1,7 @@
 import { FaLink } from "react-icons/fa6";
+import log from "../log";
 
 export default function CopyLink({ link }) {
-  console.log('link: ', link);
+  log('Möchtest du diesen Link kopieren: ', link);
   return <FaLink title="Link kopieren" onClick={() => navigator.clipboard.writeText(link)} />
 }
